2edadddc4a856beb188f9ef1990ffe8c344b7426
MyMac <mymac@MyMacs-MacBook-Pro.local>
2019-04-13 15:00:42 -0700

    letter.js push

---
+++ letter.js
@@ -0,0 +1,35 @@
+
+// LETTER CONSTRUCTOR
+
+//Constructor function for creating letter objects
+var Letter = function(character){
+    this.character = character.toUpperCase();
+    this.guessed = false;
+    //if guessed, display character, else display placeholder.
+    this.guess = function(){
+        if(this.character === ' '){
+            this.guessed = true;
+            return '   ';
+        }
+        else if(this.guessed){
+            return this.character;
+        } else {
+            return '_ ';
+        }
+    }
+    //Checks if user guess is true.
+    this.checkGuess = function(userGuess){
+        if(userGuess.toUpperCase() === this.character){
+            if(this.guessed === true){
+                console.log('You already guessed the letter ' + this.character + '!!');
+                return 'repeat';
+            } else {
+                this.guessed = true;
+                return true;
+            } 
+        }
+        this.guess();
+    }
+}
+
+module.exports = Letter;
